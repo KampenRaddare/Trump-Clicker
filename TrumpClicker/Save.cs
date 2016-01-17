8f37b3d39d567c0eab122f961661e06fa0eed97f
Sam Robinson <pinkstarsr11@gmail.com>
2016-01-16 23:43:18 -0800

    Optimize Save.cs

--- TrumpClicker/Save.cs
+++ TrumpClicker/Save.cs
@@ -1,33 +1,24 @@
 ﻿using System;
 using System.IO;
 using System.Windows;
-using System.Collections.Generic;
 
 namespace TrumpClicker
 {
-    class Save
+    internal sealed class Save
     {
 
         /// <summary>
-        /// Saves  provided data to metadata
+        /// Saves provided data to metadata
         /// </summary>
         public void WriteSaveData(int data)
         {
-            // Saving
-
-            string appData = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Trump Clicker");
-            string metaData = Path.Combine(appData, "metadata.txt");
-            List<string> file = new List<string>();
-
-            file.Add(data.ToString());
-
             try
             {
-                File.WriteAllLines(metaData, file);
+                File.WriteAllText(Meta.metaData, data.ToString());
             }
             catch (ArgumentNullException)
             {
-                MessageBox.Show("A saving error has occured . . .", "Fatal Error");
+                MessageBox.Show("A saving error has occured.", Meta.fatalError);
             }
         }
 
@@ -38,20 +29,15 @@ namespace TrumpClicker
         /// <returns></returns>
         public int ReadSaveData()
         {
-            string appData = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Trump Clicker");
-            int save;
-
-            // Load save 
             try
             {
-                save = int.Parse(File.ReadAllText(Path.Combine(appData, "metadata.txt")));
+                return int.Parse(File.ReadAllText(Meta.metaData));
             }
             catch
             {
-                MessageBox.Show("Save file is damaged\nAll progress has been lost.", "Fatal Error");
-                save = 0;
+                MessageBox.Show("Save file is damaged.\nAll progress has been lost.", Meta.fatalError);
+                return 0;
             }
-            return save;
         }
     }
 }

2777fbbfbc89ad319b9219828a36a5c8e6f10c3c
Sam Robinson <pinkstarsr11@gmail.com>
2016-01-16 17:09:11 -0800

    Initial Commit
    I only changed things that I absolutely had to change while keep similar
    class structure and having all the current desired functionalties that
    were previously not working. Would I have done more? Yes, probably, but
    this isn't my project. The main reasons the bugs were happening is
    because when you were checking for file permissions you were writing 0
    into the file that you than proceed to read from using Save(). But since
    every time 0 is wrote into it. I've removed all saving code from game as
    it is just for managing the save data. Save is where the save data is
    written and read. I thought that was what you were going for, at least.
    But it was not working like that at all before this.

--- TrumpClicker/Game.cs
+++ TrumpClicker/Game.cs
@@ -1,44 +1,15 @@
-﻿using System;
-using System.IO;
-using System.Windows;
-
-namespace TrumpClicker
+﻿namespace TrumpClicker
 {
-    class Game : BaseAbstract
+    class Game
     {
-
-        /// <summary>
-        /// Restarts game and
-        /// resets metadata to
-        /// zero.
-        /// </summary>
-        public override void SaveToMeta()
+        private int _NumberOfClicks;
+        public int NumberOfClicks
         {
-            // Restarting
-
-            string appData = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Trump Clicker");
-            string metaData = Path.Combine(appData, "metadata.txt");
-            string[] file = null;
-
-            try
+            get
             {
-                file = File.ReadAllLines(metaData);
+                return this._NumberOfClicks;
             }
-            catch (Exception)
-            {
-                MessageBox.Show("A saving error has occured . . .", "Fatal Error");
-            }
-
-            file[0] = Convert.ToString(0);
-
-            try
-            {
-                File.WriteAllLines(metaData, file);
-            }
-            catch (ArgumentNullException)
-            {
-                MessageBox.Show("A saving error has occured . . .", "Fatal Error");
-            }
+            set { this._NumberOfClicks = value; }
         }
     }
 }
